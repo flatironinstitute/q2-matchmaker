136c4cc362fe47eb513747cc733cd6236a3f76d2
mortonjt <jamietmorton@gmail.com>
2021-07-25 13:44:17 -0600

    skipping q2 tests

--- q2_matchmaker/tests/test_method.py
+++ q2_matchmaker/tests/test_method.py
@@ -31,6 +31,7 @@ class TestMatching(unittest.TestCase):
             columns=['Sex', 'Age', 'Diagnosis']
         ))
 
+    @unittest.skip('qiime2 not supported yet.')
     def test_matching(self):
         matched_metadata = matching(self.metadata, 'Diagnosis', ['Age', 'Sex'])
         matched_metadata = matched_metadata.to_dataframe()
@@ -40,6 +41,7 @@ class TestMatching(unittest.TestCase):
                       index=self.index, name='matching_id')
         )
 
+    @unittest.skip('qiime2 not supported yet.')
     def test_matching_prefix(self):
         matched_metadata = matching(
             self.metadata, 'Diagnosis', ['Age', 'Sex'], prefix='cool')
@@ -53,6 +55,7 @@ class TestMatching(unittest.TestCase):
                       index=self.index, name='matching_id')
         )
 
+    @unittest.skip('qiime2 not supported yet.')
     def test_matching_nans(self):
         self.index = pd.Index(['a1', 'a2', 'a3', 'a4',
                                'b1', 'b2', 'b3', 'b4', 'b5'],
@@ -96,6 +99,7 @@ class TestNegativeBinomialCaseControl(unittest.TestCase):
         self.table, self.metadata, self.diff = _case_control_sim(
             n=self.N, d=self.D, depth=100)
 
+    @unittest.skip('qiime2 not supported yet.')
     def test_negative_binomial_case_control(self):
         sids = [f's{i}' for i in range(self.N)]
         biom_table = biom.Table(self.table.values.T,
